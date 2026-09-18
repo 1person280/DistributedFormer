69059cfdbd2bffad5ea0a83cd14e2bd1f3d09590
1person280 <1975383276@qq.com>
2026-09-18 20:40:39 +0800

    v0.14.1: 版本号同步 (0.14.0 -> 0.14.1)

--- src/cutemamen/plugin.py
+++ src/cutemamen/plugin.py
@@ -284,7 +284,7 @@ class ExpertPlugin:
             "route": self.route,
             "lifecycle": ["on_load", "on_think", "on_unload"],
             "memory_budget": int(self.memory.footprint_bytes()),
-            "min_core_version": "0.14.0",
+            "min_core_version": "0.14.1",
             "memory_levels": list(PluginMemory.LEVELS),
             "core_version": CORE_VERSION,
             "format": "CuteMamen",
